826ebdd79e52b88abafdc346f96af49dde69a454
mkdk09 <mkdk099@gmail.com>
2020-04-22 11:17:11 +0900

    予定と出欠表の表示画面を作成

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
-'use strict'
+'use strict';
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+const User = require('../models/user');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
 
 describe('/login', () => {
     beforeAll(() => {
@@ -35,5 +38,51 @@ describe('logout', () => {
         .get('/logout')
         .expect('Location', '/')
         .expect(302);
+    });
+});
+
+describe('/schedules', () => {
+    beforeAll(() => {
+      passportStub.install(app);
+      passportStub.login({ id: 0, username: 'testuser' });
+    });
+  
+    afterAll(() => {
+      passportStub.logout();
+      passportStub.uninstall(app);
+    });
+  
+    it('予定が作成でき、表示される', (done) => {
+      User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+        request(app)
+          .post('/schedules')
+          .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
+          .expect('Location', /schedules/)
+          .expect(302)
+          .end((err, res) => {
+            let createdSchedulePath = res.headers.location;
+            request(app)
+              .get(createdSchedulePath)
+              .expect(/テスト予定1/)
+              .expect(/テストメモ1/)
+              .expect(/テストメモ2/)
+              .expect(/テスト候補1/)
+              .expect(/テスト候補2/)
+              .expect(/テスト候補3/)
+              .expect(200)
+              .end((err, res) => {
+                // テストで作成したデータを削除
+                let scheduleId = createdSchedulePath.split('/schedules/')[1];
+                Candidate.findAll({
+                  where: { scheduleId: scheduleId }
+                }).then((candidates) => {
+                  candidates.forEach((c) => { c.destroy(); });
+                  Schedule.findById(scheduleId).then((s) => { s.destroy(); });
+                });
+                if (err) return done(err);
+                done();
+              });
+          });
+      });
     });
 });
